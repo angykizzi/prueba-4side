73858be6a6d9b73b04b054e972a6690c49589f4f
Angely Escafuller <angely.escafuller@linktic.com>
2025-01-31 22:25:25 -0400

    con el modo claro y oscuro

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@ import DarkModeToggle from "@/components/DarkModeToggle.vue";
   <header
     class="w-full py-4 px-6 flex justify-between items-center bg-gray-100 dark:bg-[#1e1e1e] shadow-md"
   >
-    <h1 class="text-xl font-bold text-gray-800 dark:text-gray-200">
+    <h1 class="text-xl font-bold text-[#1f2937] dark:text-[#eaeaea]">
       Mi Aplicación
     </h1>
     <DarkModeToggle />
